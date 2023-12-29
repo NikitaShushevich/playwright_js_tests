13845d33c7710a0d6e0c21131572c7d51e7906c6
NikitaShushevich <nikita.shushevich@gmail.com>
2023-12-29 17:33:10 +0200

    first commit

---
+++ pages/home.page.js
@@ -0,0 +1,10 @@
+exports.HomePage = class HomePage{
+    constructor(page){
+        this.page = page;
+    
+        // locators
+        this.searchField = page.locator(".APjFqb");
+        
+    }
+    }
+    
---
+++ tests/search.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('Google test', async ({ page }) => {
+
+    await page.goto('https://google.com/');
+    await expect(page).toHaveTitle("Google");
+
+  });
+  
